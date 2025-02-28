e6e0571267c9fbab7108731dc4f205514d2c8282
Bogdan <bogdan.anisoiu@gmail.com>
2025-02-28 12:43:57 +0200

    citire afisare nr real

--- Anisoiu_Bogdan-Andrei_ActivitateSD2025/test1.c
+++ Anisoiu_Bogdan-Andrei_ActivitateSD2025/test1.c
@@ -7,6 +7,11 @@ int main()
 	int variabila;
 	scanf_s("%d", &variabila);
 	printf("%d", variabila);
-	return 0;
+	
 
+	printf("nr real: ");
+	float var2;
+	scanf_s("%f", &var2);
+	printf("%5.2f", var2);
+	return 0;
 }
